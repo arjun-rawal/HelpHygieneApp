e4542bdf7932afeca34005c55a05b506794fa0c2
arjun-rawal <arawalarjun@gmail.com>
2022-09-30 18:55:27 -0400

    dropdown

--- screens/RecordHours.js
+++ screens/RecordHours.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ModalDropdown from 'react-native-modal-dropdown';
+
 import {
   TouchableOpacity,
   Button,
@@ -12,13 +14,54 @@ import {
 
 
 function RecordingHours(){
+  
+
     return (
-      
     <View>
+      <Text style={styles.text1}>Activity Type</Text>
+      <ModalDropdown options={['Hygiene Drive', 'Drop Off Toiletries','Hygiene Workshop','Fundraising']}dropdownStyle={styles.dropdown1_drop}textStyle={styles.dropdown1_text}style={styles.dropdown1}></ModalDropdown>
+      <Text style={styles.text1}>Activity Date</Text>
+      <TextInput keyboardType= "numbers-and-punctuation"placeholderTextColor='red'placeholder="MM/DD/YYYY" maxLength={10} style={styles.dateinput}> </TextInput>
     </View>
         
     );
     };
-    
+const styles = StyleSheet.create({
+      dateinput:{ 
+        width:"100%",
+        borderRadius:100,
+        textAlign:'center',
+
+        borderWidth: 1,
+        padding: 10,
+      },
+      text1:{
+        paddingTop:20,
+        fontSize:25,
+        textAlign:'center',
+        justifyContent:'center',
+        alignItems:'center',
+
+      },
+      dropdown1: {
+        paddingTop:10,
+        borderWidth:0,
+        borderRadius:3,
+        borderColor:'red',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+
+      },
+      dropdown1_text:{
+        fontSize: 20,
+        color:'blue'
+
+      },
+      dropdown1_drop:{
+        width:115,
+      }
+
+    });   
 
 export default RecordingHours;
--- screens/ViewLoggedHours.js
+++ screens/ViewLoggedHours.js
@@ -1,10 +1,29 @@
-import { Button, StyleSheet, Text, TextInput, View,Image, Dimensions } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
 
-function ViewLoggedHours(){
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Dimensions,
+} from "react-native";
+import { color } from '@rneui/base';
 
-
-    return(
-        <Text>LoggedHours</Text>
-    )
-}
+function ViewLoggedHours() {
+  return (
+  <View>
+  <Text>LoggedHours</Text>  
+  <ModalDropdown options={['option 1', 'option 2']}style={styles.container}></ModalDropdown>
+  </View>
+  );
+  }
 export default ViewLoggedHours;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50
+  },
+
+});
